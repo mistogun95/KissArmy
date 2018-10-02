640a8c9baaf6b0d6b1847aee64e3833162a0ccd5
mistogun95 <daniel.iacopino@gmail.com>
2018-10-02 15:23:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/fairy/kissarmy/StartApp.java
@@ -0,0 +1,13 @@
+package com.example.fairy.kissarmy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_app);
+    }
+}
