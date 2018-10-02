b9eb8089d1073a939a19e51eb4a8c7e57ad969c0
mistogun95 <daniel.iacopino@gmail.com>
2018-10-02 16:46:28 +0200

    Aggiunto pulsante di ricerca (non funzionante)

--- app/src/main/java/com/example/fairy/kissarmy/StartApp.java
+++ app/src/main/java/com/example/fairy/kissarmy/StartApp.java
@@ -2,6 +2,8 @@ package com.example.fairy.kissarmy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class StartApp extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class StartApp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_app);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
